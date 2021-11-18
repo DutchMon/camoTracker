1cc6cdb0644860683a1bdfdd86ac135b4ff26751
DutchMon <dutch.holland13@gmail.com>
2021-11-18 14:38:14 -0700

    forgot to add new files

---
+++ camo-tracker/components/layoutWeapons.js
@@ -0,0 +1,34 @@
+/*------------ Imports ---------*/
+
+import Navbar from './navbar'
+import Head from 'next/head'
+import Header from './Header'
+import PropTypes from 'prop-types'
+
+
+/*------- Default Page Layout ------*/
+
+const Layout = ({ children }) => (
+	<>
+		<Head>
+			<title>Camo Tracker</title>
+		</Head>
+		<div className="dashboard is-full-height">
+			<Navbar></Navbar>
+			<div className="dashboard-main is-scrollable" id="noBackgroundImage">
+				<Header />
+				<div className="block flexGrow scroll contentHeight">
+					<main>
+						<div className="container">{children}</div>
+					</main>
+				</div>
+			</div>
+		</div>
+	</>
+)
+export default Layout
+
+Layout.propTypes = {
+	children: PropTypes.node,
+}
+
---
+++ camo-tracker/public/guns/index.js
@@ -0,0 +1,17 @@
+import STG from './assaultRifles/STG-44_Gunsmith_VG.png'
+import AS44 from './assaultRifles/AS44_Gunsmith_VG.png'
+import Automaton from './assaultRifles/Automaton_Gunsmith_VG.png'
+import BAR from './assaultRifles/BAR_Gunsmith_VG.png'
+import ITRA from './assaultRifles/ITRA_Burst_Gunsmith_VG.png'
+import NZ41 from './assaultRifles/NZ-41_Gunsmith_VG.png'
+import Volk from './assaultRifles/Volk.png'
+
+export {
+    STG,
+    AS44,
+    Automaton,
+    BAR,
+    ITRA,
+    NZ41,
+    Volk
+}
